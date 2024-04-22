f034496b6b31555de4d6943885b9eeacbb499a01
sharonpraju <sharonpraju@gmail.com>
2024-04-23 02:29:45 +0530

    :sparkles: Status fetching & major fixes

--- index.js
+++ index.js
@@ -8,15 +8,16 @@ const key_gen = require("./lib/keygen");
  * @constructor
  * @param {string} PHONEPE_MERCHANT_ID - The unique identifier of the merchant on PhonePe.
  * @param {string} PHONEPE_MERCHANT_USER_ID - The unique identifier of the merchant user on PhonePe.
- * @param {string} PHONEPE_CALLBACK_URL - The callback URL where PhonePe will send transaction notifications.
- * @param {string} PHONEPE_KEY - The secret key used for secure communication with PhonePe APIs.
+ * @param {string} PHONEPE_API_KEY - The secret key used for secure communication with PhonePe APIs.
+ * @param {string} PHONEPE_HOST_URL - The base url to interact with payment gateway APIs.
  */
 class PhonePe {
-  constructor(PHONEPE_MERCHANT_ID, PHONEPE_MERCHANT_USER_ID, PHONEPE_CALLBACK_URL, PHONEPE_KEY) {
+  constructor(PHONEPE_MERCHANT_ID, PHONEPE_MERCHANT_USER_ID, PHONEPE_API_KEY, PHONEPE_API_KEY_INDEX, PHONEPE_HOST_URL) {
     this.merchantId = PHONEPE_MERCHANT_ID;
     this.merchantUserId = PHONEPE_MERCHANT_USER_ID;
-    this.phonepeCallbackUrl = PHONEPE_CALLBACK_URL;
-    this.phonepeKey = PHONEPE_KEY;
+    this.phonepeKey = PHONEPE_API_KEY;
+    this.phonepeKeyIndex = PHONEPE_API_KEY_INDEX;
+    this.phonepeHostUrl = PHONEPE_HOST_URL;
   }
 
   /**
@@ -28,19 +29,40 @@ class PhonePe {
    * @returns {Promise<string>} The generated or existing transaction ID.
    */
 
-  async createTxn() {
+  async generateTransactionId() {
     this.tnxId = await key_gen();
     return this.tnxId;
   }
-  base64gen(payload) {
+
+  generateBase64(payload) {
     let base64 = Buffer.from(JSON.stringify(payload)).toString("base64");
     return base64;
   }
-  sha256gen(base64) {
-    let preformatter = base64 + "/pg/v1/pay" + this.phonepeKey;
-    let sha256Data = sha256(preformatter);
-    sha256Data = sha256Data + "###" + 1;
-    return sha256Data;
+
+  generateSignature(base64, endPoint) {
+    let preformatter = base64 + endPoint + this.phonepeKey;
+    let hash = sha256(preformatter);
+    let signature = hash + "###" + this.phonepeKeyIndex;
+    return signature;
+  }
+
+  async sendRequest(headers, url, method, requestData) {
+    return new Promise(async (resolve, reject) => {
+      const options = {
+        method: method,
+        url: url,
+        headers: headers,
+        data: { request: requestData }
+      };
+      try {
+        let { data } = await axios.request(options);
+        resolve(data.data);
+      } catch (error) {
+        console.log(error.response)
+        console.trace(`Request Failed with status ${error?.response?.data?.code}. Refer https://developer.phonepe.com/v1/reference`);
+        reject(`${error?.response?.data?.code} - ${error?.response?.data?.message}`)
+      }
+    });
   }
 
   /**
@@ -56,49 +78,72 @@ class PhonePe {
    * @returns {Promise<object>} An object containing the generated transaction details.
    */
 
-  async generate(data) {
+  async generatePaymentUrl(data) {
     return new Promise(async (resolve, reject) => {
-      if (!data) {
-        reject("Data Cannot be empty");
-      }
-      if (!data.amount) {
-        reject("Amount is required in payload");
-      }
+      if (!data) return reject("Transaction data cannot be empty");
+      if (!data.amount) return reject("amount is required in payload");
+      if (!data.callbackUrl) return reject("callbackUrl is required in payload");
+      if (!data.redirectUrl) return reject("redirectUrl is required");
+      if (!data.paymentInstrumentType) return reject("paymentInstrumentType is required");
+      if (!this.merchantId) return reject("merchantId is required");
+      if (!this.merchantUserId) return reject("merchantUserId is required");
+      if (!this.phonepeKey) return reject("phonepeKey is required");
+      if (!this.phonepeHostUrl) return reject("phonepeHostUrl is required");
+
       let payload = {
         merchantId: this.merchantId,
         merchantUserId: this.merchantUserId,
-        callbackUrl: this.phonepeCallbackUrl,
-        amount: data.amount,
+        callbackUrl: data.callbackUrl,
+        amount: Number(data.amount) * 100,
         mobileNumber: data.mobileNumber,
-        merchantTransactionId: data.transactionId ? data.transactionId : await this.createTxn(),
+        merchantTransactionId: data.transactionId ? data.transactionId : await this.generateTransactionId(),
         redirectUrl: data.redirectUrl,
-        redirectMode: data.redirectMode,
+        redirectMode: data.redirectMode ? data.redirectMode : "REDIRECT",
         paymentInstrument: {
-          type: data.paymentInstrumentType,
+          type: data.paymentInstrumentType ? data.paymentInstrumentType : "PAY_PAGE",
         },
       };
-      let requestData = this.base64gen(payload);
-      let signature = this.sha256gen(requestData);
-      let getURI = await this.sendRequest(signature, requestData);
-      resolve(getURI);
+      let requestData = this.generateBase64(payload);
+      let endPoint = "/pg/v1/pay";
+      let signature = this.generateSignature(requestData, endPoint);
+      let url = this.phonepeHostUrl + endPoint;
+      let method = "POST";
+      let headers = {
+        "Content-Type": "application/json",
+        "X-VERIFY": signature
+      }
+      try {
+        let transaction_data = await this.sendRequest(headers, url, method, requestData);
+        resolve(transaction_data);
+      } catch (error) {
+        reject(error)
+      }
     });
   }
-  async sendRequest(signature, requestData) {
+
+  async getTransactionStatus(data) {
     return new Promise(async (resolve, reject) => {
-      const options = {
-        method: "POST",
-        url: "https://api-preprod.phonepe.com/apis/pg-sandbox/pg/v1/pay",
-        headers: {
-          "Content-Type": "application/json",
-          "X-VERIFY": signature,
-        }
-      };
+      if (!data) return reject("Transaction data cannot be empty");
+      if (!data.transactionId) return reject("transactionId is required");
+      if (!this.merchantId) return reject("merchantId is required");
+      if (!this.merchantUserId) return reject("merchantUserId is required");
+      if (!this.phonepeKey) return reject("phonepeKey is required");
+      if (!this.phonepeHostUrl) return reject("phonepeHostUrl is required");
+
+      let endPoint = `/pg/v1/status/${this.merchantId}/${data.transactionId}`;
+      let signature = this.generateSignature("", endPoint);
+      let url = this.phonepeHostUrl + endPoint;
+      let method = "GET";
+      let headers = {
+        "Content-Type": "application/json",
+        "X-VERIFY": signature,
+        "X-MERCHANT-ID": this.merchantId
+      }
       try {
-        let { data } = await axios.request(options, { data: { request: requestData } });
-        resolve(data.data);
+        let transaction_data = await this.sendRequest(headers, url, method, null);
+        resolve(transaction_data);
       } catch (error) {
-        console.trace(`Request Failed with status ${error?.response?.data?.code}. Refer https://developer.phonepe.com/v1/reference/pay-api`);
-        reject(`Transaction failed - ${error?.response?.data?.code}`)
+        reject(error)
       }
     });
   }
--- readme.md
+++ readme.md
@@ -24,47 +24,54 @@ To get started with the PhonePe Payments Gateway integration, follow these steps
 const PhonePe = require("phonepe-kit");
 ```
 
-2. Create an instance of the PhonePe class by providing your merchant key, merchant ID, callback URL, and secret key:
+2. Create an instance of the PhonePe class by providing your merchant key, merchant ID, secret key and host url (test or production):
 
 ```javascript
 const initilizePhonePe = new PhonePe(
-  "YOUR_MERCHANT_ID",
-  "YOUR_MERCHANT_USER_ID",
-  "YOUR_CALLBACK_URL",
-  "YOUR_SECRET_KEY"
+  "MERCHANT_ID",
+  "MERCHANT_USER_ID",
+  "API_KEY",
+  "API_KEY_INDEX",
+  "HOST_URL"
 );
 ```
 
-3. Generate the PAY_PAGE link by providing the amount and calling the generate() method:
+3. Generate the PAY_PAGE link by providing the amount and calling the generatePaymentUrl() method:
 
 ```javascript
-let generate_data = {
+let transactionData = {
   amount: 100,
-  returnUrl: "https://example.com/return-url",
-  mobileNumber: "+91234567890",
+  transactionId: "ABCDEFG12345"
+  callbackUrl: "https://example.com/callback-url",
+  redirectUrl: "https://example.com/return-url",
   redirectMode: "GET",
+  mobileNumber: "+91234567890",
   paymentInstrumentType: "PAY_PAGE"
 };
 
-initilizePhonePe.generate(generate_data).then((data) => {
-  //payment url will in `data`
+initilizePhonePe.generatePaymentUrl(transactionData).then((data) => {
+  //payment url will be in `data`
 });
 
 OR
 
 try{
-  let payment_url =  await initilizePhonePe.generate(generate_data);
+  let data =  await initilizePhonePe.generatePaymentUrl(transactionData);
+  //payment url will be in `data`
 } catch (error){
   console.log(error);
 }
 ```
 
+Note: If `transactionId` is not passed it will be generated automatically.
+
 ### Parameters
 
-    YOUR_MERCHANT_ID: Your PhonePe Payments Gateway merchant key.
-    YOUR_MERCHANT_USER_ID: Your PhonePe Payments Gateway merchant ID.
-    YOUR_CALLBACK_URL: The URL to which PhonePe will send the payment response callback.
-    YOUR_SECRET_SALT_KEY: Your secret key for secure communication with PhonePe.
+    MERCHANT_ID: Your PhonePe Payments Gateway merchant key.
+    MERCHANT_USER_ID: Your PhonePe Payments Gateway merchant ID.
+    SECRET_SALT_KEY: Your secret key for secure communication with PhonePe.
+    HOST_URL: The base url to interact with payment gateway APIs.
+    callbackUrl: The URL to which PhonePe will send the payment response callback.
 
 ## Sample Code
 
@@ -72,17 +79,24 @@ try{
 const PhonePe = require("phonepe-kit");
 
 const initilizePhonePe = new PhonePe(
-  "PGTESTPAYUAT80",
-  "YOUR_SECRET_SALT_KEY",
-  "https://callbackurl.com",
-  "YOUR_SECRET_SALT_KEY"
+  "ABCDEF",
+  "abcdef123",
+  "12345-45679-sadfg-asdfgh",
+  "1",
+  "https://api-preprod.phonepe.com/apis/hermes"
 );
 
-let generate_data = {
+let transactionData = {
   amount: 100,
+  transactionId: "ABCDEFG12345",
+  callbackUrl: "https://example.com/callback-url",
+  redirectUrl: "https://example.com/return-url",
+  redirectMode: "REDIRECT",
+  mobileNumber: "+91234567890",
+  paymentInstrumentType: "PAY_PAGE"
 };
 
-initilizePhonePe.generate(generate_data).then((data) => {
+initilizePhonePe.generatePaymentUrl(transactionData).then((data) => {
   console.log(data);
 });
 ```
